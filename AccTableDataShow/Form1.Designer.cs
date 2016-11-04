eaf407aa501ce5ed9b07a7400d866b33178c6d3a
xiang li <xiang li>
2016-11-04 22:05:12 +0800

    First commit

---
+++ AccTableDataShow/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace AccTableDataShow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Data File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(44, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(630, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dataGridView1_RowStateChanged);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(44, 256);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.LegendText = "Acceleration(rad/s2)";
+            series1.Name = "Acc";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.LegendText = "Velocity(rad/s)";
+            series2.Name = "Vel";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Legend = "Legend1";
+            series3.LegendText = "Displacement(rad)";
+            series3.Name = "Dis";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(932, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(44, 583);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(932, 300);
+            this.chart2.TabIndex = 3;
+            this.chart2.Text = "chart2";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(215, 36);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(42, 16);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "FFT";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(743, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(743, 146);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(743, 194);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(867, 101);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(867, 146);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 21);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(867, 194);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 21);
+            this.textBox6.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(783, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Max";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(906, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Min";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(714, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Acc";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(714, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 12);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Vel";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(714, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 12);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Dis";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(323, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 12);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Frequency";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(387, 15);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 21);
+            this.textBox7.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(345, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 12);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Phase";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(387, 42);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 21);
+            this.textBox8.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(492, 18);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 12);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "SNR";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(521, 15);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 21);
+            this.textBox9.TabIndex = 20;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(492, 45);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(23, 12);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Amp";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(521, 42);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 21);
+            this.textBox10.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1030, 925);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AccTableDataShow";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
+
---
+++ AccTableDataShow/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace AccTableDataShow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string FilePath;
+        public DataTable dt;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All files (*.*)|*.*|txt files (*.txt)|*.txt";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = openFileDialog.FileName;
+            }
+            read_data();
+            dataGridView1.DataSource = dt;
+            show_data();
+            if(checkBox1.Checked)
+                show_data_fft();
+        }
+
+        private void read_data()
+        {
+            dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[] {
+                new DataColumn("Time",typeof(float)),
+                new DataColumn("Acceleration",typeof(float)),
+                new DataColumn("Velocity",typeof(float)),
+                new DataColumn("Displacement",typeof(float)),
+                new DataColumn("ADC_Data",typeof(string))
+            });
+            DataRow dr = null;
+
+            StreamReader sr = new StreamReader(FilePath);
+            while (sr.Peek() > -1)
+            {
+                dr = dt.NewRow();
+                string line = sr.ReadLine();
+                if (line.Contains("Hz"))
+                {
+                    textBox7.Text = line.Replace(" ", "");
+                }
+                if (line.Contains("°"))
+                {
+                    textBox8.Text = line.Split(':')[1].Replace(" ", "");
+                }
+                if (line.Contains("dB"))
+                {
+                    textBox9.Text = line.Split(':')[1].Replace(" ", "");
+                }
+                if (line.Contains("Amp"))
+                {
+                    textBox10.Text = line.Split(':')[1].Replace(" ", "");
+                }
+                if (line.Contains("|") && line[0] != 'T')
+                {
+                    string[] array = line.Split('|');
+                    dr["Time"] = float.Parse(array[0]);
+                    dr["Acceleration"] = float.Parse(array[1]);
+                    dr["Velocity"] = float.Parse(array[2]);
+                    dr["Displacement"] = float.Parse(array[3]);
+                    dr["ADC_Data"] = array[4];
+
+                    dt.Rows.Add(dr);
+                }
+            }
+        }
+        private void show_data()
+        {
+            ZoomToggle(chart1, true);
+            List<double> ls_acc = get_data_colomn("Acceleration");
+            List<double> ls_vel = get_data_colomn("Velocity");
+            List<double> ls_dis = get_data_colomn("Displacement");
+            textBox1.Text = ls_acc.Max().ToString();
+            textBox4.Text = ls_acc.Min().ToString();
+            textBox2.Text = ls_vel.Max().ToString();
+            textBox5.Text = ls_vel.Min().ToString();
+            textBox3.Text = ls_dis.Max().ToString();
+            textBox6.Text = ls_dis.Min().ToString();
+            chart1.Series["Acc"].Points.DataBindY(ls_acc);
+            chart1.Series["Vel"].Points.DataBindY(ls_vel);
+            chart1.Series["Dis"].Points.DataBindY(ls_dis);
+        }
+
+        private void show_data_fft()
+        {
+            ZoomToggle(chart2, true);
+            float Fs = 1 / float.Parse(dt.Rows[0]["Time"].ToString());
+            double[] x = get_data_colomn("Acceleration").ToArray();
+            int n = x.Length;
+            double[] freqs = new Double[n / 2];
+            for (int i = 0; i < freqs.Length; i++)
+            {
+                freqs[i] = Fs * i * 0.5 / freqs.Length;
+            }
+            complex[] y = new complex[n];//接收复数结果的数组 
+            double[] z = new Double[n];//接收幅值结果的数组 
+            y = airthm.dft(x, n);
+            z = airthm.amplitude(y, n);
+            double[] z_half = new Double[n / 2];
+            Array.ConstrainedCopy(z, 0, z_half, 0, n / 2);
+            chart2.Series["Series1"].Points.DataBindXY(freqs, z_half);
+        }
+
+        private List<double> get_data_colomn(string ColomnName)
+        {
+            List<double> ls = new List<double>();  //存放一整列所有的值 
+            foreach (DataRow dr in dt.Rows)
+            {
+                ls.Add(float.Parse(dr[ColomnName].ToString()));
+            }
+            return ls;
+        }
+
+        private void ZoomToggle(System.Windows.Forms.DataVisualization.Charting.Chart mychart, bool Enabled)
+        {
+            // Enable range selection and zooming end user interface
+            mychart.ChartAreas[0].CursorX.IsUserEnabled = Enabled;
+            mychart.ChartAreas[0].CursorX.IsUserSelectionEnabled = Enabled;
+            mychart.ChartAreas[0].CursorX.Interval = 0;
+            mychart.ChartAreas[0].AxisX.ScaleView.Zoomable = Enabled;
+            mychart.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+            mychart.ChartAreas[0].AxisX.ScrollBar.ButtonStyle = System.Windows.Forms.DataVisualization.Charting.ScrollBarButtonStyles.ResetZoom;
+            mychart.ChartAreas[0].AxisX.ScaleView.SmallScrollMinSize = 0;
+
+            mychart.ChartAreas[0].CursorY.IsUserEnabled = Enabled;
+            mychart.ChartAreas[0].CursorY.IsUserSelectionEnabled = Enabled;
+            mychart.ChartAreas[0].CursorY.Interval = 0;
+            mychart.ChartAreas[0].AxisY.ScaleView.Zoomable = Enabled;
+            mychart.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+            mychart.ChartAreas[0].AxisY.ScrollBar.ButtonStyle = System.Windows.Forms.DataVisualization.Charting.ScrollBarButtonStyles.ResetZoom;
+            mychart.ChartAreas[0].AxisY.ScaleView.SmallScrollMinSize = 0;
+            if (Enabled == false)
+            {
+                //Remove the cursor lines
+                mychart.ChartAreas[0].CursorX.SetCursorPosition(double.NaN);
+                mychart.ChartAreas[0].CursorY.SetCursorPosition(double.NaN);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                show_data_fft();
+            else
+                chart2.Series["Series1"].Points.Clear();
+        }
+
+        private void dataGridView1_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+            e.Row.HeaderCell.Value = string.Format("{0}", e.Row.Index + 1);
+        }
+    }
+
+    /// <summary>
+    /// 快速傅立叶变换(Fast Fourier Transform)。
+    /// </summary>
+    struct complex//定义复数 
+    {
+
+        //复数a+bi中 
+        //a为实部，b为虚部 
+        public double a;
+        public double b;
+        public static complex commul(double x, complex y)
+        {
+            complex c = new complex();
+            c.a = x * y.a;
+            c.b = x * y.b;
+
+            return c;
+        }
+        public static complex commulcc(complex x, complex y)
+        {
+            complex c = new complex();
+            c.a = x.a * y.a - x.b * y.b;
+            c.b = x.a * y.b + x.b * y.a;
+
+            return c;
+        }
+        public static complex comsum(complex x, complex y)
+        {
+            complex c = new complex();
+            c.a = x.a + y.a;
+            c.b = x.b + y.b;
+
+            return c;
+        }
+        public static complex comsum1(double x, complex y)
+        {
+            complex c = new complex();
+            c.a = x + y.a;
+            c.b = y.b;
+
+            return c;
+        }
+        public static complex decrease(complex x, complex y)
+        {
+            complex c = new complex();
+            c.a = x.a * y.a - x.b * y.b;
+            c.b = x.a * y.b + x.b * y.a;
+
+            return c;
+        }
+        public static complex powcc(complex x, double n)
+        {
+            int k;
+            complex xout;
+            xout.a = 1;
+            xout.b = 0;
+            for (k = 1; k<= n; k++) 
+                {
+                xout = complex.commulcc(xout, x);
+            }
+            return xout;
+        }
+    }
+    class airthm
+    {
+        //计算ω=exp（j*2*pi/n） 
+        public static complex omega(int n)
+        {
+            complex x;
+            x.a = Math.Cos(0 - 2 * Math.PI / n);
+            x.b = Math.Sin(0 - 2 * Math.PI / n);
+            return x;
+        }
+        public static complex[] dft(double[] signal, int n)  //(信号，信号长度) 
+        {
+            int i, j;
+            complex w1;
+            w1 = omega(n);
+            complex[] w = new complex[n];
+            for (i = 0; i<n; i++) 
+                {
+                w[i] = complex.powcc(w1, i);
+            }
+            complex[] f = new complex[n];
+            complex temp;  //w[i]的次方 
+            complex temp1; //f中单项的值 
+            for (i = 0; i <n; i++) 
+                {
+                f[i].a = 0;
+                f[i].b = 0;
+                for (j = 0; j <n; j++) 
+                        {
+                    temp = complex.powcc(w[i], j);
+                    temp1 = complex.commul(signal[j], temp);
+                    f[i] = complex.comsum(f[i], temp1);
+                }
+            }
+            return f;
+        }
+        //求幅值  x  信号  n  信号长度  返回 幅值数组 
+        public static double[] amplitude(complex[] x, int n)
+        {
+            int i;
+            double temp;
+            double[] amp = new double[n];
+            for (i = 0; i< n; i++) 
+                {
+                temp = x[i].a * x[i].a + x[i].b * x[i].b;
+                amp[i] = Math.Sqrt(temp);
+            }
+            return amp;
+        }
+    }
+
+}
